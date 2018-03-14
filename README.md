3f422bbcdb592ee3fd0ba2205103299c473018e7
gasolin <gasolin@gmail.com>
2018-03-15 00:35:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,67 @@
+<img src='https://d3vv6lp55qjaqc.cloudfront.net/items/263e3q1M2Y2r3L1X3c2y/helmet.png'/>
+
+# Gitcoin
+
+Gitcoin pushes Open Source Forward.  Learn more at [https://gitcoin.co](https://gitcoin.co)
+
+[Star](https://github.com/gitcoinco/chrome_ext/stargazers) and [watch](https://github.com/gitcoinco/chrome_ext/watchers) this github repository to stay up to date, we're pushing new code several times per week!
+
+## Get It Today
+
+The [Chrome extension](https://gitcoin.co/extensions/chrome) is live. Go get it now.
+
+<a href="https://gitcoin.co/extension">
+<img src="https://d3vv6lp55qjaqc.cloudfront.net/items/062d2k3t43040c2u2e2H/Image%202017-09-25%20at%204.12.56%20AM.png"/>
+</a>
+
+## What Does This Extension Do
+
+### Browse Work Directly on Github
+
+<img src='readme/browse_on_github.png'>
+
+### Filter Available Work from Github
+
+<img src='readme/filter_available_work.png'>
+
+### Fund Work Directly on Github
+
+<img src='readme/fund.png'>
+
+### Tip Users Direclty on Github
+
+<img src='readme/tip.png'>
+
+
+# How to build the extension
+
+Clone the repo, then
+
+```sh
+$ yarn install
+$ yarn build
+```
+
+# Legal
+
+```
+    Copyright (C) 2017 Gitcoin Core
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+```
+
+
+<!-- Google Analytics -->
+<img src='https://ga-beacon.appspot.com/UA-102304388-1/gitcoinco/chrome_ext' style='width:1px; height:1px;' >
---
+++ src/components/History.js
@@ -0,0 +1,175 @@
+import React, { Component } from 'react';
+import { BOUNTIES_BASE_URL, limitStr, timeDifference } from '../utils/utils';
+import { DEVELOPMENT } from '../constants';
+
+const bountyStyle = {
+  float: 'right'
+};
+
+const searchBtnStyle = {
+  marginLeft: '25px'
+};
+
+const bountyImgStyle = {
+  height: '20px',
+  width: '20px'
+};
+
+function BountyItem({item}) {
+  return (
+    <tr>
+      <td><img src={item.imgsrc} alt='' style={bountyImgStyle} /></td>
+      <td>{item.timeDiff}</td>
+      <td>{item.tokenName}</td>
+      <td>{item.title}</td>
+      <td>{item.tokenName}</td>
+      <td>{item.state}</td>
+      <td><a href={item.linkUrl} target= '_blank' className='target' rel='noopener noreferrer'>'View >></a></td>
+    </tr>
+  )
+}
+
+function TableNodes({bounties, loading}) {
+  if (loading) {
+    return (
+      <tbody>
+        <tr>
+          <td col='4'>Loading...</td>
+        </tr>
+      </tbody>
+    );
+  }
+  if (bounties.length === 0) {
+    return (
+      <tbody>
+        <tr>
+          <td col='4'>No Bounties Found</td>
+        </tr>
+      </tbody>
+    );
+  }
+
+  var max_display = 10;
+  var lines = [];
+  for(var i=0; i < bounties.length && i < max_display; i++) {
+    let bounty = bounties[i];
+    let item = {
+      imgsrc: bounty['avatar_url'],
+      timeDiff: timeDifference(new Date(), new Date(bounty['web3_created'])),
+      tokenName: Math.round(100.0 * bounty['value_in_token'] / 10 ** 18) / 100 + ' ' + bounty['token_name'],
+      title: limitStr(bounty['title'], 30),
+      state: limitStr(bounty['status'],30),
+      linkUrl: bounty['github_url']
+    }
+    lines.push(item);
+  }
+
+  return (
+    <tbody>
+      {lines.map(item => (<BountyItem item={item} key={item.title}/>))}
+    </tbody>
+  )
+}
+
+export class History extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bounties: [],
+      browserLocation: null,
+      githubUsername: null,
+      keyword: undefined,
+      loading: true,
+      fetchError: null
+    }
+  }
+
+  componentDidMount() {
+    let url = localStorage['browser_location'];
+    let keyword = localStorage['keyword'];
+    let isOnGitHubcom = typeof url !== 'undefined' && url.indexOf('://github.com') !== -1 && url.indexOf('://github.com') < 15;
+    let isOnRepo = typeof url !== 'undefined' && isOnGitHubcom && url.match(/.+\/.+\/.+\/.+\/?/gi) != null;
+
+    if (isOnGitHubcom && isOnRepo) {
+      var repo = url.split('/')[4];
+      this.searchBounties(repo);
+    }
+
+    if (keyword) {
+      this.setState({keyword});
+      this.searchBounties(keyword);
+    }
+
+    if(localStorage['githubusername']) {
+      this.setState({
+        githubUsername: localStorage['githubusername'],
+        browserLocation: url
+    });
+    }
+
+    let fetchUrl = DEVELOPMENT ? '/mock.json' :
+      BOUNTIES_BASE_URL + 'idx_status=open&order_by=-web3_created';
+    fetch(fetchUrl)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          bounties: data,
+          loading: false
+        });
+      })
+      .catch((e) => {
+        this.setState({fetchError: 'Error: Could not reach api: ' + e});
+      });
+  }
+
+  searchBounties(keyword) {
+    keyword = keyword.toLowerCase();
+    var matching_bounties = [];
+    var all_bounties = this.state.bounties;
+    for (var i = all_bounties.length - 1; i >= 0; i--) {
+      var bounty_keywords = JSON.parse(all_bounties[i].raw_data[8]).issueKeywords.toLowerCase();
+      var bounty_title = all_bounties[i].title.toLowerCase();
+      var do_keywords_contain = bounty_keywords.indexOf(keyword) !== -1;
+      var does_title_contain = bounty_title.indexOf(keyword) !== -1;
+      if (do_keywords_contain || does_title_contain) {
+        matching_bounties.push(all_bounties[i]);
+        localStorage.setItem('keyword', keyword);
+      }
+    }
+    this.setState({
+      bounties: matching_bounties,
+      keyword,
+      loading: false
+    });
+  }
+
+  render() {
+    let href = this.state.githubusername ? `https://gitcoin.co/funding/new?&user=${this.state.githubUsername}&source=${this.state.browserLocation}` : 'https://gitcoin.co/funding/new';
+    return this.state.fetchError ? (<span>{this.state.fetchError}</span>) : (
+      <div id='history'>
+        <div id='bounty' style={bountyStyle}>
+            <a target='_blank' href={href} rel='noopener noreferrer' className='btn btn-sm btn-primary js-details-target gitcoin_button'>+ Fund Issue</a>
+        </div>
+        <h5>Funded Issues</h5>
+        <input type='text' id='search_bar' value={this.state.keyword} placeholder='Search for keywords..' />
+        <button style={searchBtnStyle} className='btn btn-sm btn-primary js-details-target gitcoin_button search-button'>Search</button>
+        <table className='table table-striped' id='openbounties'>
+          <thead>
+            <tr>
+              <th></th>
+              <th>When</th>
+              <th>Amount</th>
+              <th>For</th>
+              <th>Status</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <TableNodes {...this.state} />
+        </table>
+        <a target='new' href='https://gitcoin.co/explorer' rel='noopener noreferrer'>View More &gt;&gt; </a>
+      </div>
+    );
+  }
+}
+
+export default History;
